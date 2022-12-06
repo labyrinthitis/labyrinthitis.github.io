d4792b7902376b909eb35a994a9a449f787e256b
labyrinthitis <114704834+labyrinthitis@users.noreply.github.com>
2022-12-06 04:39:13 -0800

    vercel github pages config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-  /**
-   * Tell Next.js where the `public` folder is.
-   * Replace `nextjs-github-pages` with your Github repo project name.
-   */
-  assetPrefix: isProd ? "./" : "",
-  /**
-   * Disable server-based image optimization.
-   *
-   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
-   */
-  images: {
-    unoptimized: true,
-  },
-};
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+ const nextConfig = {
+  basePath: './',
+}
+
+module.exports = nextConfig
