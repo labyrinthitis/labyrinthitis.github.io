7bae1271c79b31df2f3c99dd0b1bbc7afb8e9389
labyrinthitis <114704834+labyrinthitis@users.noreply.github.com>
2022-12-06 03:51:46 -0800

    initial commit

---
+++ components/layout.tsx
@@ -0,0 +1,59 @@
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+const name = 'labyrinthitis'
+export const siteTitle = 'labyrinthitis'
+
+export default function Layout({
+  children,
+  home
+}: {
+  children: React.ReactNode
+  home?: boolean
+}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <meta
+          name="description"
+          content="a technical blog"
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              {/* <Image
+                priority
+                src="/images/profile.jpg"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt={name}
+              /> */}
+            </Link>
+            {/* <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2> */}
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  )
+}
